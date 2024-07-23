39b580cd14317e32268ae33731c411024c005256
Akinfolami Akin-Alamu <aoa9@cornell.edu>
2024-07-22 20:15:23 -0500

    Spotify setup complete

--- main.go
+++ main.go
@@ -1,5 +1,110 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/gin-gonic/gin"
+	"github.com/zmb3/spotify/v2"
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+const redirectURI = "http://localhost:8080/callback"
+
+var (
+	auth = spotifyauth.New(
+		spotifyauth.WithRedirectURL(redirectURI),
+		spotifyauth.WithScopes(spotifyauth.ScopePlaylistReadPrivate, spotifyauth.ScopePlaylistModifyPrivate, spotifyauth.ScopeUserReadPrivate),
+	)
+	state  = "abc123"
+	client *spotify.Client
+	mux    sync.RWMutex
+)
+
+func setClient(c *spotify.Client) {
+	mux.Lock()
+	defer mux.Unlock()
+	client = c
+}
+
+func getClient() *spotify.Client {
+	mux.RLock()
+	defer mux.RUnlock()
+	return client
+}
+
+func authMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if getClient() == nil {
+			url := auth.AuthURL(state)
+			c.Redirect(http.StatusTemporaryRedirect, url)
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
+
+func authRedirectHandler(c *gin.Context) {
+	token, err := auth.Token(c, state, c.Request)
+	if err != nil {
+		c.JSON(http.StatusForbidden, gin.H{"error": "Couldn't get token"})
+	}
+	// create a client using the specified token
+	client := spotify.New(auth.Client(c, token))
+	setClient(client)
+
+	user, err := client.CurrentUser(context.Background())
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Internal Server Error",
+		})
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Welcome " + user.DisplayName,
+	})
+}
+
+func getPlaylists(c *gin.Context) {
+	client := getClient()
+	playlists, err := client.CurrentUsersPlaylists(context.Background())
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Internal Server Error",
+		})
+		log.Println(err)
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"message": "success",
+		"data":    playlists,
+	})
+}
+
 func main() {
 
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World!",
+		})
+	})
+
+	r.GET("/callback", authRedirectHandler)
+	r.GET("/playlists", authMiddleware(), getPlaylists)
+
+	url := auth.AuthURL(state)
+	fmt.Println("Please log in to Spotify by visiting the following page in your browser:\n\n", url)
+
+	err := r.Run(":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
